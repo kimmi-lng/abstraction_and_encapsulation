c7ee61b0a789778490935aeefdcb550d6e30f90f
Kim Russel <kimrussel0718@gmail.com>
2026-06-03 01:04:40 +0800

    forgot to mention that i also added the getter method to access the data members.

--- prac_prog_fan_class/fan_class.py
+++ prac_prog_fan_class/fan_class.py
@@ -1,14 +1,17 @@
 class Fan:
+
     #constants
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+
     #instance variables
     def __init__(self, speed=SLOW, on=False, radius=5, color='blue'):
         self._speed = int(speed)
         self._on = bool(on)
         self._radius = float(radius)
         self._color = str(color)
+
     #getter method
     def get_speed(self):
         return self._speed
@@ -18,4 +21,5 @@ class Fan:
         return self._radius
     def get_color(self):
         return self._color
+    
     #setter method
