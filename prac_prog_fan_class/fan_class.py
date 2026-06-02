5c1ddc8959c75afffe88276b191ca739dd1f896d
Kim Russel <kimrussel0718@gmail.com>
2026-06-03 00:48:35 +0800

    changed  one variable name.

--- prac_prog_fan_class/fan_class.py
+++ prac_prog_fan_class/fan_class.py
@@ -4,9 +4,9 @@ class Fan:
     MEDIUM = 2
     FAST = 3
     #instance variables
-    def __init__(self, speed=SLOW, is_on=False, radius=5, color='blue'):
+    def __init__(self, speed=SLOW, on=False, radius=5, color='blue'):
         self.__speed = int(speed)
-        self.__is_on = bool(is_on)
+        self.__on = bool(on)
         self.__radius = float(radius)
         self.__color = str(color)
     #getter method
