21c5e40148bb5077608b6a3042c9670df5347030
Kim Russel <kimrussel0718@gmail.com>
2026-06-03 00:35:13 +0800

    added the instance variables.

--- prac_prog_fan_class/fan_class.py
+++ prac_prog_fan_class/fan_class.py
@@ -3,6 +3,7 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
-    def __init__(self):
+    #instance variables
+    def __init__(self, speed=SLOW, is_on=False, radius=5, color='blue'):
     #getter method
     #setter method
