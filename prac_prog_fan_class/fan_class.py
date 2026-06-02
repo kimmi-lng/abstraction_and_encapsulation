8cb6fbeb1a33adcb64c5de802f564a29892c8ee5
Kim Russel <kimrussel0718@gmail.com>
2026-06-03 00:38:32 +0800

    added the privitized data.

--- prac_prog_fan_class/fan_class.py
+++ prac_prog_fan_class/fan_class.py
@@ -5,5 +5,9 @@ class Fan:
     FAST = 3
     #instance variables
     def __init__(self, speed=SLOW, is_on=False, radius=5, color='blue'):
+        self.__speed = int(speed)
+        self.__is_on = bool(is_on)
+        self.__radius = float(radius)
+        self.__color = str(color)
     #getter method
     #setter method
