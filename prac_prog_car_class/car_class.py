dd95bdb7209539d495f1bfe6bffa1449757c2a85
Kim Russel <kimrussel0718@gmail.com>
2026-06-03 10:57:05 +0800

    added the instance  variables

--- prac_prog_car_class/car_class.py
+++ prac_prog_car_class/car_class.py
@@ -1,2 +1,5 @@
 class Car():
-    def __init__(self): 
+    def __init__(self, year_model, make, speed=0):
+        self._year_model = year_model
+        self._make = make
+        self._speed = speed
