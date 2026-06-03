f37c7a7eca282ecc44bbdf6b23e98ff1e689ced1
Kim Russel <kimrussel0718@gmail.com>
2026-06-04 00:19:25 +0800

    i just realized that i wont be following the basic rules if i will be using a camel case on my method, so i added another class istead called TestFan then changes the method to run()

--- prac_prog_fan_class/test_fan.py
+++ prac_prog_fan_class/test_fan.py
@@ -1,25 +1,23 @@
 from fan_class import Fan
 
-def TestFan():
+class TestFan():
 
-#For the first object, assign the maximum speed, radius 10, color yellow, and turn it on.
-    fan1 = Fan(Fan.FAST, True, 10, 'yellow')
+    def run():
+        #For the first object, assign the maximum speed, radius 10, color yellow, and turn it on.
+        fan1 = Fan(Fan.FAST, True, 10, 'yellow')
 
-#Assign medium speed, radius 5, color blue, and turn it off for the second object.
-    fan2 = Fan(Fan.MEDIUM, False, 5, 'blue')
+        #Assign medium speed, radius 5, color blue, and turn it off for the second object.
+        fan2 = Fan(Fan.MEDIUM, False, 5, 'blue')
 
-#Display each object’s speed, radius, color, and on properties.
-    print('Fan 1 properties:')
-    print(f'Speed: {fan1.get_speed()}')
-    print(f'Radius: {fan1.get_radius()}')
-    print(f'Color: {fan1.get_color()}')
-    print(f'On: {fan1.get_on()}')
+        #Display each object’s speed, radius, color, and on properties.
+        print('Fan 1 properties:')
+        print(f'Speed: {fan1.get_speed()}')
+        print(f'Radius: {fan1.get_radius()}')
+        print(f'Color: {fan1.get_color()}')
+        print(f'On: {fan1.get_on()}')
 
-    print('\nFan 2 properties:')
-    print(f'Speed: {fan2.get_speed()}')
-    print(f'Radius: {fan2.get_radius()}')
-    print(f'Color: {fan2.get_color()}')
-    print(f'On: {fan2.get_on()}')
-
-if __name__ == "__main__":
-    TestFan()
+        print('\nFan 2 properties:')
+        print(f'Speed: {fan2.get_speed()}')
+        print(f'Radius: {fan2.get_radius()}')
+        print(f'Color: {fan2.get_color()}')
+        print(f'On: {fan2.get_on()}')
