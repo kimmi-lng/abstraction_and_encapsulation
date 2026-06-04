ecc4e032d41b3f6c1ce927a71db2c3d06a4d8a86
Kim Russel <kimrussel0718@gmail.com>
2026-06-04 16:36:50 +0800

    added the condition for acceleration

--- prac_prog_car_class/test_car.py
+++ prac_prog_car_class/test_car.py
@@ -1,5 +1,12 @@
 from car_class import Car
 
 class TestCar:
+
     def run(self):
         car = Car(1992, "McLaren F1")
+        print(f'TESTING CAR: {car.get_year_model()}{car.get_make()}')
+        
+        print("ACCELERATING--")
+        for i in range(1, 6):
+            car.accelerate()
+            print(f"Car current speed after {i}: {car.get_speed()} Kph")
