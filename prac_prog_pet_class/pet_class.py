b4593570a0a554733c615b5f144dab160d16ff81
Kim Russel <kimrussel0718@gmail.com>
2026-06-04 16:57:13 +0800

    as what the problem says, i created a class named Pet then added its data attributes in private.

---
+++ prac_prog_pet_class/pet_class.py
@@ -0,0 +1,5 @@
+class Pet:
+    def __init__(self, name, animal_type, age):
+        self.__name = name #name of pet
+        self.__animal_type = animal_type #type of animal
+        self.__age = age #for pets age
