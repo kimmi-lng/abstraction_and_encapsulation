29a298ce4bf06de34ff60f82952374c0ead18edb
Kim Russel <kimrussel0718@gmail.com>
2026-06-04 17:16:38 +0800

    used the getters to display the data

--- prac_prog_pet_class/test_pet.py
+++ prac_prog_pet_class/test_pet.py
@@ -3,7 +3,7 @@ from pet_class import Pet
 class TestPet:
     def run(self):
         pet1 = Pet()
-        print("INPUT PET DETAILS:")
+        print("INPUT PET DETAILS:\n")
 
         name = str(input("Enter pet's name: "))
         type_of_animal = str(input("Enter pet's type of animal: "))
@@ -12,3 +12,8 @@ class TestPet:
         pet1.set_name(name)
         pet1.set_animal_type(type_of_animal)
         pet1.set_age(age)
+
+        print("\nPET PROFILE:")
+        print(f"Name: {pet1.get_name()}")
+        print(f"Type of animal: {pet1.get_animal_type()}")
+        print(f"Age: {pet1.get_age()} years old")   
