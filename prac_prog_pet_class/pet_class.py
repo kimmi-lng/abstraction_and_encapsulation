11eb6102a41395be43eafac9827c48364a272dcc
Kim Russel <kimrussel0718@gmail.com>
2026-06-04 17:07:28 +0800

    changed on the data attributes because it will cause problem, then added the getter and setter method

--- prac_prog_pet_class/pet_class.py
+++ prac_prog_pet_class/pet_class.py
@@ -1,5 +1,25 @@
 class Pet:
-    def __init__(self, name, animal_type, age):
-        self.__name = name #name of pet
-        self.__animal_type = animal_type #type of animal
-        self.__age = age #for pets age
+    def __init__(self):
+        self.__name = "" #name of pet
+        self.__animal_type = "" #type of animal
+        self.__age = 0 #for pets age
+
+    #setter method
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
+
+    #getter method
+    def get_name(self):
+        return self.__name
+
+    def get_animal_type(self):
+        return self.__animal_type
+
+    def get_age(self):
+        return self.__age
