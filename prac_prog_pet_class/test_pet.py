6ff56777c97f3d3b4d8d88114da047914b3cf4f2
Kim Russel <kimrussel0718@gmail.com>
2026-06-04 17:14:27 +0800

    used the setters to store the data

--- prac_prog_pet_class/test_pet.py
+++ prac_prog_pet_class/test_pet.py
@@ -8,3 +8,7 @@ class TestPet:
         name = str(input("Enter pet's name: "))
         type_of_animal = str(input("Enter pet's type of animal: "))
         age = int(input("Enter pet's age: "))
+
+        pet1.set_name(name)
+        pet1.set_animal_type(type_of_animal)
+        pet1.set_age(age)
